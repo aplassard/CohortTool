99881d97d36fb3bbc91247cbf244825f6dce65ac
aplassard <andrew.plassard@gmail.com>
2013-02-07 16:35:17 -0500

    Cleaned up imports

--- src/org/bmi/cchmc/cohorttool/util/ServletUtilities.java
+++ src/org/bmi/cchmc/cohorttool/util/ServletUtilities.java
@@ -1,14 +1,9 @@
 package org.bmi.cchmc.cohorttool.util;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
+
 import java.net.MalformedURLException;
 import java.net.URLConnection;
 import java.net.URLEncoder;
